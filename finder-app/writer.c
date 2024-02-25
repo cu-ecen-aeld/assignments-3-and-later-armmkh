4ada8dfa213cb089b907596639015c2b03a23cf2
armmkh <armkh89@gmail.com>
2024-02-25 12:12:52 +0100

    full-test success

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -3,29 +3,30 @@
 
 int main(int argc, char *argv[])
 {
+    openlog("Logs", LOG_PID, LOG_USER);
     if (argc != 3)
     {
         printf("This script expects two arguments.");
+        syslog(LOG_ERR, "Insufficient arguments.");
         return 1;
     }
     char *filename = argv[1];
 
-    openlog("Logs", LOG_PID, LOG_USER);
     // open the file for writing
     FILE *fp = fopen(filename, "w");
     if (fp == NULL)
     {
         printf("Error opening the file %s", filename);
+        syslog(LOG_ERR, "Error opening the file %s", argv[1]);
+
         return -1;
     }
     fprintf(fp, "%s", argv[2]);
-    syslog(LOG_DEBUG, "Writing %s{0} to %s{1}" , argv[1] ,argv[2] );
-
+    syslog(LOG_DEBUG, "Writing %s{0} to %s{1}", argv[1], argv[2]);
 
     // close the file
     fclose(fp);
     closelog();
 
     return 0;
-
 }
